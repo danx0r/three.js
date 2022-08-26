5cdc1e4e1ae9466592783803cce9c93e9aaac53d
danx0r <dan.miller@eye0.com>
2022-08-26 00:27:07 -0700

    fixed

--- pybrotest/boiler.py
+++ pybrotest/boiler.py
@@ -1,13 +1,5 @@
-from test import main
+from testlib import main
 try:
-    from pyodide import create_proxy
-    from js import THREE, document, window
-    renderer = THREE.WebGLRenderer.new()
-    container = document.querySelector('#container')
-    container.append(renderer.domElement)
-    plat = "pyscript"
-    main(THREE, renderer, window, create_proxy)
-except:
     from browser import document, window
     THREE = window.THREE
     renderer = THREE.WebGLRenderer.new()
@@ -15,4 +7,12 @@ except:
     def create_proxy(p):
         return p
     plat = "brython"
+    main(THREE, renderer, window, create_proxy)
+except:
+    from pyodide import create_proxy
+    from js import THREE, document, window
+    renderer = THREE.WebGLRenderer.new()
+    container = document.querySelector('#container')
+    container.append(renderer.domElement)
+    plat = "pyscript"
     main(THREE, renderer, window, create_proxy)
